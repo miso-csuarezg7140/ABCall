cc981fe3d8d06a2a6f30f4490259ee3d1839b61b
dafermiso <df.guerrerov1@uniandes.edu.co>
2024-10-21 13:57:05 -0500

    Se crea la estructura para la vista de register

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -2,14 +2,17 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AuthComponent } from './auth.component';
 import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
 
 @NgModule({
   imports: [
     CommonModule
   ],
   declarations: [AuthComponent,
-                 LoginComponent
+                 LoginComponent,
+                 RegisterComponent
   ],
-  exports: [LoginComponent]
+  exports: [LoginComponent,
+            RegisterComponent]
 })
 export class AuthModule { }
